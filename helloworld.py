65d4c48ab16ac1ebc4b4a7dc8b63df613d71f051
NNNMM12345 <nikolai1177@outlook.com>
2018-03-16 16:59:51 -0700

    My first repository

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+'''first_name = input("Enter First Name:")
+last_name = input("Enter Last Name:")
+full_name = first_name+" "+last_name
+number = eval(input())
+#birth = int(input("Enter Birth Year:"))   # input for int in python
+print("My name is " +full_name)
+print("My birth year is ",number)
+print(type(number))'''
+
+# exercise 26
+
